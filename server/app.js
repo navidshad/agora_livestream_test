4ae8eb84db2934300949a4857cde00396ce62ba2
Navid Shad <navidshad72@gmail.com>
2021-02-09 01:43:11 -0500

    server - add getToken endpoint.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var express = require('express');
-var {RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole} = require('agora-access-token')
+var { RtcTokenBuilder, RtmTokenBuilder, RtcRole, RtmRole } = require('agora-access-token')
 
 var PORT = 3000;
 
@@ -19,7 +19,33 @@ app.set('port', PORT);
 app.use(express.favicon());
 app.use(app.router);
 
-var generateRtcToken = function(req, resp) {
+const channels = [];
+
+function addChannelToken(channelName, token) {
+    let index = -1;
+
+    index = channels.findIndex((channel) => channel.name == channelName);
+
+    if (index == -1) {
+        channels.push({ token, name: channelName });
+    }
+    else {
+        channels[index].token = token;
+    }
+}
+
+function findChannelToken(channelName) {
+    index = channels.findIndex((channel) => channel.name == channelName);
+
+    if (index == -1) {
+        return null;
+    }
+    else {
+        return channels[index].token;
+    }
+}
+
+var generateRtcToken = function (req, resp) {
     var currentTimestamp = Math.floor(Date.now() / 1000)
     var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
     var channelName = req.query.channelName;
@@ -31,31 +57,37 @@ var generateRtcToken = function(req, resp) {
 
 
     var key = RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channelName, uid, role, privilegeExpiredTs);
+    addChannelToken(channelName, key);
 
     resp.header("Access-Control-Allow-Origin", "*")
-        //resp.header("Access-Control-Allow-Origin", "http://ip:port")
+
     return resp.json({ 'key': key }).send();
 };
 
-var generateRtmToken = function(req, resp) {
+var getToken = function (req, resp) {
     var currentTimestamp = Math.floor(Date.now() / 1000)
     var privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds
-    var account = req.query.account;
-    if (!account) {
-        return resp.status(400).json({ 'error': 'account is required' }).send();
+    var channelName = req.query.channelName;
+    if (!channelName) {
+        return resp.status(400).json({ 'error': 'channel name is required' }).send();
     }
 
-    var key = RtmTokenBuilder.buildToken(appID, appCertificate, account, RtmRole, privilegeExpiredTs);
+    var key = findChannelToken(channelName);
 
     resp.header("Access-Control-Allow-Origin", "*")
-        //resp.header("Access-Control-Allow-Origin", "http://ip:port")
-    return resp.json({ 'key': key }).send();
+
+    if (key) {
+        return resp.json({ 'key': key }).send();
+    }
+    else {
+        resp.status(400).json({ 'error': 'channel dosen\'t found.' }).send();
+    }
 };
 
 app.get('/rtcToken', generateRtcToken);
-app.get('/rtmToken', generateRtmToken);
+app.get('/getToken', getToken);
 
 
-http.createServer(app).listen(app.get('port'), function() {
+http.createServer(app).listen(app.get('port'), function () {
     console.log('AgoraSignServer starts at ' + app.get('port'));
 });
